0079accbe3ff5632d8bee306eb271f6dd4805f98
chaosprint <chaosprint@gmail.com>
2022-02-03 17:35:25 +0100

    publish as native es module

--- src/vite-plugins-cross-origin-isolation.mjs
+++ src/vite-plugins-cross-origin-isolation.mjs
@@ -1,10 +1,3 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
 const crossOriginIsolation = () => ({
   name: 'configure-server',
 
@@ -18,5 +11,4 @@ const crossOriginIsolation = () => ({
 
 });
 
-var _default = crossOriginIsolation;
-exports.default = _default;
+export default crossOriginIsolation
